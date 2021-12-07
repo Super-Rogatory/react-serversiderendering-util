8b9e67850dbf7c87e32bc04485a24df5b09c2005
super-rogatory <chukwudi10@gmail.com>
2021-12-06 21:37:37 -0800

    used isomorphic-fetch and window global object to share information between client and server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Grid from './Grid';
 
 class App extends React.Component {
 	render() {
-		return <div>{'Hello World'}</div>;
+		return (
+			<div>
+				<Grid data={this.props.data} />
+			</div>
+		);
 	}
 }
 
--- server/index.js
+++ server/index.js
@@ -13,31 +13,32 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-app.get('*', async (req, res, next) => {
-	try {
-		const data = await fetchPopularRepos();
-		if (!data) throw new Error('failed to fetch information from encoded URI');
-		const markup = renderToString(<App data={data} />);
-
-		res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Server-Side Rendering</title>
-          <script src='/bundle.js' defer></script>
-          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-        </head>
-        <body>
-          <div id="app">${markup}</div>
-        </body>
-      </html>
-  `);
-	} catch (err) {
-		return res.status(500).send({ msg: 'mission failed' });
-	}
+app.get('*', (req, res, next) => {
+	fetchPopularRepos()
+		.then((data) => {
+			const markup = renderToString(<App data={data} />);
+			res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Server-Side Rendering</title>
+            <script src='/bundle.js' defer></script>
+            <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+          </head>
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+    `);
+		})
+		.catch((err) => next(err));
 });
 
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Something went wrong!');
+});
 app.listen(PORT, () => console.log(`server is listening on PORT ${PORT}`));
